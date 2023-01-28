fb82c31b51e532ca8ca79d05817292c51fa50436
Devaansh Kumar <devaansh840@LAPTOP-1GJ1OTPU.localdomain>
2023-01-28 21:08:06 +0530

    created log out functionality

--- app.js
+++ app.js
@@ -3,29 +3,67 @@ const sq = require("./config/database");
 const Users = require("./models/users");
 const Locations = require("./models/locations");
 const Logs = require("./models/log");
+const crypto = require("crypto");
 
 const app = express();
 const PORT = process.env.PORT | 3000;
 app.use(express.urlencoded({ extended: true }));
-// I get the user details as url query parameters from the user when he scans along with random string
 
-app.get('/',(req,res)=>{
-  res.sendFile(__dirname + '/public/index.html')
-})
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+
+// login post request will contain rollno and uniqueId in the request body
 app.post("/login", async (req, res) => {
   const data = req.body;
   console.log(data);
-  // const student = await Users.findByPk(data.rollNo);
-  const student = await Users.findOne({where: {rollNo: data.rollNo}});
-  const location = await Locations.findOne({where: {uniqueId: data.uniqueId}});
-  if(student && location){
-    const logs = Logs.create({location: location.location, rollNo: data.rollNo, loginTime: new Date().toISOString().slice(0, 19).replace('T', ' ')});
+  const student = await Users.findOne({ where: { rollNo: data.rollNo } });
+  const location = await Locations.findOne({
+    where: { uniqueId: data.uniqueId },
+  });
+
+  if (student && location) {
+    const token = crypto.randomBytes(8).toString("hex");
+    const logs = Logs.create({
+      location: location.location,
+      rollNo: data.rollNo,
+      loginTime: new Date().toISOString().slice(0, 19).replace("T", " "),
+      token: token,
+    });
+
+    res.json({ msg: "You are logged in!", token });
   }
-  res.json({msg:"You are logged in!"});
 });
 
-app.post("/logout", (req, res) => {});
+
+// logout patch request will contain the user roll number, log out location and token in the request body
+app.patch("/logout", async (req, res) => {
+  const data = req.body;
+  console.log(data);
+
+  const student = await Users.findOne({ where: { rollNo: data.rollNo } });
+  const location = await Locations.findOne({
+    where: { uniqueId: data.uniqueId },
+  });
+  const log = await Logs.findOne({
+    where: { token: data.token },
+  });
+
+  const token = log.dataValues.token;
+  const rollNo = student.dataValues.rollNo;
+  const userLog = await Logs.findOne({
+    where: { rollNo: rollNo, token: token },
+  });
+  if (student && location && token && userLog) {
+    await Logs.update(
+      { logoutTime: new Date().toISOString().slice(0, 19).replace("T", " ") },
+      { where: { /*rollNo: student,*/ token: token } }
+      );
+      res.json({ msg: "You are logged out!" });
+  }
+});
 
 
 const start = async () => {
--- models/log.js
+++ models/log.js
@@ -1,24 +1,32 @@
 const Sequelize = require("sequelize");
 const sq = require("../config/database");
 
-const Logs = sq.define("logs", {
-  rollNo: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    primarKey: true,
+const Logs = sq.define(
+  "logs",
+  {
+    rollNo: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      primarKey: true,
+    },
+    location: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    loginTime: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    logoutTime: {
+      type: Sequelize.DATE,
+      allowNull: true,
+    },
+    token: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
   },
-  location: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  loginTime: {
-    type: Sequelize.DATE,
-    allowNull: false,
-  },
-  logoutTime: {
-    type: Sequelize.DATE,
-    allowNull: true,
-  },
-});
+  { timestamps: false }
+);
 
 module.exports = Logs;
